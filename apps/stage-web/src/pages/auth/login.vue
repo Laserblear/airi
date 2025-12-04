cdcdc48d5acf8d9e117f4c86c59c0fe8bac4963e
RainbowBird <git@luoling.moe>
2025-12-05 04:54:38 +0800

    feat: login with google

---
+++ apps/stage-web/src/pages/auth/login.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { Button } from '@proj-airi/ui'
+import { createAuthClient } from 'better-auth/client'
+
+async function signIn() {
+  const authClient = createAuthClient({
+    baseURL: import.meta.env.VITE_SERVER_URL || 'http://localhost:3000',
+  })
+
+  const data = await authClient.signIn.social({
+    provider: 'google',
+  })
+
+  console.log(data)
+}
+</script>
+
+<template>
+  <div>
+    <h1>Login</h1>
+
+    <Button @click="signIn">
+      Sign In
+    </Button>
+  </div>
+</template>
