fc15a390718ff0d6561b794843b52281c5764dc1
RainbowBird <git@luoling.moe>
2025-12-05 23:37:12 +0800

    feat: github

--- apps/stage-web/src/pages/auth/login.vue
+++ apps/stage-web/src/pages/auth/login.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { Button } from '@proj-airi/ui'
+import { useRouter } from 'vue-router'
 import { toast } from 'vue-sonner'
 
 import { API_SERVER_URL, authClient } from '../../composables/auth'
 
-async function signIn() {
+const router = useRouter()
+
+async function signIn(type: 'google' | 'github') {
   const { error, data } = await authClient.signIn.social({
-    provider: 'google',
-    callbackURL: `${API_SERVER_URL}/api/auth/callback/google`,
+    provider: type,
+    callbackURL: `${API_SERVER_URL}/api/auth/callback/${type}`,
   })
 
   if (error) {
@@ -15,17 +18,23 @@ async function signIn() {
   }
 
   if (data && data.redirect && data.url) {
-    window.open(data.url, '_blank')
+    // window.open(data.url, '_blank')
+    router.replace(data.url)
   }
 }
 </script>
 
 <template>
   <div>
-    <h1>Login</h1>
 
-    <Button @click="signIn">
-      Sign In
-    </Button>
+    <div class="flex flex-row gap-2">
+      <Button @click="signIn('google')">
+        Sign In with Google
+      </Button>
+
+      <Button @click="signIn('github')">
+        Sign In with GitHub
+      </Button>
+    </div>
   </div>
 </template>
